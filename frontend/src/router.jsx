4e8453120a33426334405012349f186e77f6a048
Guy Parintorn <parintorn-y@rmutp.ac.th>
2026-07-09 01:48:54 +0700

    fix: redirect home route

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Navigate, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import ProtectedRoute from "./shared/components/ProtectedRoute";
 import CarListPage from "./pages/CarListPage";
@@ -6,6 +6,7 @@ import CarListPage from "./pages/CarListPage";
 function AppRouter() {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/cars" replace />} />
       <Route path="/login" element={<LoginPage />} />
       <Route
         path="/cars"
